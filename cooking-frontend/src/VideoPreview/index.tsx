a4c31e29756fafef490de87c96092dc70aba7e78
realningzheng <zning@nd.edu>
2024-02-15 11:17:15 -0500

    make video preview responsive

--- cooking-frontend/src/VideoPreview/index.tsx
+++ cooking-frontend/src/VideoPreview/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ReactPlayer from 'react-player'
-import './style.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../reducers';
 
@@ -13,12 +12,29 @@ export default function VideoPreview(props: VideoPreviewProps) {
     const isPlaying = useSelector((state: RootState) => state.videoControlReducer.isPlaying);
 
     return (
-        <div className="video-text-overlay-container">
+        <div
+            // className="video-text-overlay-container"
+            style={{
+                position: "relative",
+                width: "100%",
+                height: 0,
+                paddingTop: '56.25%'
+            }}
+        >
             <ReactPlayer
-                className="react-player"
+                // className="react-player"
                 ref={videoPlayerRef}
                 url={props.vurl}
                 playing={isPlaying}
+                style={{
+                    position: "absolute",
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                }}
+                width={'100%'}
+                height={'100%'}
                 controls
             />
         </div>
